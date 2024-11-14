32d46aebeebb959bad5f6899fd016a1a2f65ad06
Nicholas Brown <njbrown@wm.edu>
2024-11-13 23:17:19 -0500

    Finished Tutorial

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
 
+function addTask(){
+    
+    const taskInput = document.getElementById("taskInput").value;
+
+    if(taskInput){
+        const newTask = document.createElement('li');
+        newTask.innerText = taskInput;
+        const list = document.getElementById("taskList")
+        list.appendChild(newTask)
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className="button";
+        deleteButton.innerText="x";
+        newTask.appendChild(deleteButton)
+
+        deleteButton.addEventListener("click", () => list.removeChild(newTask))
+
+        document.getElementById('taskInput').value=''
+    }
+}
